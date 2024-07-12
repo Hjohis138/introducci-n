21545911d6b295577932031ee94e906eaab0df77
Johana Herrera <hjohis@gmail.com>
2024-07-11 19:20:05 -0500

    variable

--- ejericicio1.py
+++ ejericicio1.py
@@ -1,4 +1,17 @@
-print("el nombre del producto es : "+ input("¿cual tu animal favorito?: ")+" "+ input("¿Cuál es tu color favorito : "))
-
+#print("el nombre del producto es : "+ input("¿cual tu animal favorito?: ")+" "+ input("¿Cuál es tu color favorito : "))
+#nombre="johana"
+#print(nombre)
+#edad=42
+#print(edad)
+#edad1=50
+#edad2=20
+#suma=edad1+edad2
+#print(suma)
+#nombre_usuario=input("dime tu nombre: ")
+#print("Tu nomnbre es: " + nombre_usuario)
+nombre1="Johana"
+nombre2="Alexandra"
+nombres=nombre1 + " " + nombre2
+print(nombres)
 
       
