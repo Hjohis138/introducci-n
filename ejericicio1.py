c4090ee2daee35d17b09fa4ad655afff5bb5eeb6
Johana Herrera <hjohis@gmail.com>
2024-07-11 19:53:03 -0500

    trabajo 2

--- ejericicio1.py
+++ ejericicio1.py
@@ -9,9 +9,22 @@
 #print(suma)
 #nombre_usuario=input("dime tu nombre: ")
 #print("Tu nomnbre es: " + nombre_usuario)
-nombre1="Johana"
-nombre2="Alexandra"
-nombres=nombre1 + " " + nombre2
-print(nombres)
+#nombre1="Johana"
+#nombre2="Alexandra"
+#nombres=nombre1 + " " + nombre2
+#print(nombres)
+#nombre1="Tony Soprano"
+#edad=51
+#print(nombre1,edad)
+
+#nombre="Johana"
+#apellido="Herrera"
+#nombre_apellido=nombre  + " " + apellido
+#print(nombre_apellido)
+
+#curso="python"
+print("Estas tomando un curso de :  "  + curso)
+
+
 
       
